a5326a861782f5849b1fe52daf17ee590c69fadc
Felipe Sousa <felipe.fps09@hotmail.com>
2022-01-11 21:21:00 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,34 @@
-import Pagination from '../../components/Navbar/Pagination';
+import Pagination from '../../components/Pagination';
+
 import './styles.css'
+import MovieCard from 'components/MovieCard';
 const Listing = () => {
     return (
         <>
-        <Pagination />
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    
+                </div>
+            </div>
+
+
         </>
-        );
+    );
 }
 export default Listing;
