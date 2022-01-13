1aaf9d0bf2d2831320b05179ffb69a55822f3cce
Felipe Sousa <felipe.fps09@hotmail.com>
2022-01-13 19:37:47 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -0,0 +1,75 @@
+package com.devsuperior.dsmovie.dto;
+
+import com.devsuperior.dsmovie.entities.Movie;
+
+public class MovieDTO {
+
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;
+
+	public MovieDTO() {
+
+	}
+
+	public MovieDTO(Long id, String title, Double score, Integer count, String image) {
+
+		id = id;
+		title = title;
+		score = score;
+		count = count;
+		image = image;
+	}
+
+	public MovieDTO(Movie movie) {
+
+		this.id = movie.getId();
+		this.title = movie.getTitle();
+		this.score = movie.getScore();
+		this.count = movie.getCount();
+		this.image = movie.getImage();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+}
