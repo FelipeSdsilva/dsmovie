b73c3ff0ea20b2e6ab1d63135139535f38a14cd5
Felipe Sousa <felipe.fps09@hotmail.com>
2022-01-13 22:29:24 -0300

    First homolog

--- backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -15,7 +15,7 @@ public class ScorePK implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@ManyToOne
-	@JoinColumn(name = "move_id")
+	@JoinColumn(name = "movie_id")
 	private Movie movie;
 	
 	@ManyToOne
