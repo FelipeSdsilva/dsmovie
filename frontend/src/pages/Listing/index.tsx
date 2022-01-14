2124bc2f7ef099b45b1c393f83a10f3844a3bb76
Felipe Sousa <felipe.fps09@hotmail.com>
2022-01-14 19:24:54 -0300

    Firt request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,7 +2,16 @@ import Pagination from '../../components/Pagination';
 
 import './styles.css'
 import MovieCard from 'components/MovieCard';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
+
 const Listing = () => {
+
+    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+        .then(response =>{
+            console.log(response.data);
+        });
+
     return (
         <>
             <Pagination />
@@ -23,7 +32,7 @@ const Listing = () => {
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    
+
                 </div>
             </div>
 
