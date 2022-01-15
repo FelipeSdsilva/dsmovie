62a585fb8d3e911d7c4afd2651768fe617f750f2
Felipe Sousa <felipe.fps09@hotmail.com>
2022-01-15 11:37:03 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,13 +1,18 @@
 import MovieStars from 'components/MovieStars';
 import './styles.css';
 
-function MovieScore() {
-    const score = 3.15;
-    const count = 13; 
+type Props = {
+    score: number,
+    count: number
+}
+
+function MovieScore({ score, count }: Props) {
+
+
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -40,12 +40,13 @@ const Listing = () => {
             <Pagination />
             <div className="container">
                 <div className="row">
-                    {page.content.map(movie => (
+                    {page.content.map(movie => 
+                    (
                         <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                             <MovieCard movie={movie} />
                         </div>
-
-                    ))};
+                    )
+                    )}
 
                 </div>
             </div>
